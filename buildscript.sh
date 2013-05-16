3cca988d4a4782a0196ee49eee5d52f8f59c6693
ka_shrinivaasan <ka_shrinivaasan@158c0cd9-b3a7-4c9b-a7e4-89ab9697128b>
2013-05-16 10:02:13 +0000

    Modified buildscript with option for multiprocessing added to make commandline

--- buildscript.sh
+++ buildscript.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
-
-#/***************************************************************************************
-#VIRGO - a linux module extension with CPU and Memory pooling with cloud capabilities
-#Copyright (C) 2009-2013  Ka.Shrinivaasan
-#
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
-#
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#mail to: ka.shrinivaasan@gmail.com
-#*****************************************************************************************/
-
-make mrproper
-make O=/home/kashrinivaasan/linux-3.7.8-build oldconfig
-make O=/home/kashrinivaasan/linux-3.7.8-build prepare
-make O=/home/kashrinivaasan/linux-3.7.8-build outputmakefile
-make O=/home/kashrinivaasan/linux-3.7.8-build archprepare
-make O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=scripts
-make O=/home/kashrinivaasan/linux-3.7.8-build modules
-make O=/home/kashrinivaasan/linux-3.7.8-build
+make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build clean 
+make -j 2 mrproper
+make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build oldconfig
+make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build prepare
+make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build outputmakefile
+make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build archprepare
+make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=scripts
+make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=drivers/virgo/cpupooling/virgocloudexec clean
+make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=drivers/virgo/cpupooling/virgocloudexec
+make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build modules_install SUBDIRS=drivers/virgo/cpupooling/virgocloudexec
+make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build modules
+make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build
 make O=/home/kashrinivaasan/linux-3.7.8-build modules_install
 make O=/home/kashrinivaasan/linux-3.7.8-build install
 
