de8f4a0022e4fcf36e0e451bae13ba3d52abbf01
ka_shrinivaasan <ka_shrinivaasan@158c0cd9-b3a7-4c9b-a7e4-89ab9697128b>
2013-05-22 16:44:05 +0000

    Increased concurrency to 5

--- buildscript.sh
+++ buildscript.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build clean 
-make -j 2 mrproper
-make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build oldconfig
-make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build prepare
-make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build outputmakefile
-make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build archprepare
-make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=scripts
-make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=drivers/virgo/cpupooling/virgocloudexec clean
-make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=drivers/virgo/cpupooling/virgocloudexec
-make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build modules_install SUBDIRS=drivers/virgo/cpupooling/virgocloudexec
-make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build modules
-make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build
-make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build modules_install
-make -j 2 O=/home/kashrinivaasan/linux-3.7.8-build install
-make -j 2 headers_install
+make -j 5 mrproper
+make -j 5 O=/home/kashrinivaasan/linux-3.7.8-build oldconfig
+make -j 5 O=/home/kashrinivaasan/linux-3.7.8-build prepare
+make -j 5 O=/home/kashrinivaasan/linux-3.7.8-build outputmakefile
+make -j 5 O=/home/kashrinivaasan/linux-3.7.8-build archprepare
+make -j 5 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=scripts
+make -j 5 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=drivers/virgo/cpupooling/virgocloudexec clean
+make -j 5 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=drivers/virgo/cpupooling/virgocloudexec
+make -j 5 O=/home/kashrinivaasan/linux-3.7.8-build modules_install SUBDIRS=drivers/virgo/cpupooling/virgocloudexec
+make -j 5 O=/home/kashrinivaasan/linux-3.7.8-build modules
+make -j 5 O=/home/kashrinivaasan/linux-3.7.8-build
+make -j 5 O=/home/kashrinivaasan/linux-3.7.8-build modules_install
+make -j 5 O=/home/kashrinivaasan/linux-3.7.8-build install
+make -j 5 headers_install
 
 
