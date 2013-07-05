53da63fa2947b0a6c5c627bd12cbc787022d0237
ka_shrinivaasan <ka_shrinivaasan@158c0cd9-b3a7-4c9b-a7e4-89ab9697128b>
2013-07-05 06:22:14 +0000

    Added more automation to build script: -Module.symvers and .config copying from previous build -cleaning up old build residues -update-initramfs for loading the modules after build is over

--- buildscript.sh
+++ buildscript.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
-make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build clean 
+#make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build clean 
+#make mrproper
+#make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build oldconfig
+#make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build prepare
+#make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build outputmakefile
+#make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build archprepare
+#make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=scripts
+#make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=drivers/virgo/cpupooling/virgocloudexec clean
+#make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=drivers/virgo/cpupooling/virgocloudexec
+#make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build modules_install SUBDIRS=drivers/virgo/cpupooling/virgocloudexec
+#make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build modules
+#make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build
+#make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build modules_install
+#make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build install
+#make headers_install
+
+rm -rf /home/kashrinivaasan/linux-3.7.8-build-2/*
+cp /home/kashrinivaasan/linux-3.7.8-build/.config /home/kashrinivaasan/linux-3.7.8-build-2/
+cp /home/kashrinivaasan/linux-3.7.8-build/Module.symvers /home/kashrinivaasan/linux-3.7.8-build-2/
+make -j 20 O=/home/kashrinivaasan/linux-3.7.8-build-2 clean 
 make mrproper
-make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build oldconfig
-make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build prepare
-make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build outputmakefile
-make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build archprepare
-make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=scripts
-make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=drivers/virgo/cpupooling/virgocloudexec clean
-make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build modules SUBDIRS=drivers/virgo/cpupooling/virgocloudexec
-make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build modules_install SUBDIRS=drivers/virgo/cpupooling/virgocloudexec
-make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build modules
-make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build
-make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build modules_install
-make -j 6 O=/home/kashrinivaasan/linux-3.7.8-build install
+make -j 20 O=/home/kashrinivaasan/linux-3.7.8-build-2 oldconfig
+make -j 20 O=/home/kashrinivaasan/linux-3.7.8-build-2 prepare
+make -j 20 O=/home/kashrinivaasan/linux-3.7.8-build-2 outputmakefile
+make -j 20 O=/home/kashrinivaasan/linux-3.7.8-build-2 archprepare
+make -j 20 O=/home/kashrinivaasan/linux-3.7.8-build-2 modules SUBDIRS=scripts
+make -j 20 O=/home/kashrinivaasan/linux-3.7.8-build-2 modules SUBDIRS=drivers/virgo/cpupooling/virgocloudexec clean
+make -j 20 O=/home/kashrinivaasan/linux-3.7.8-build-2 modules SUBDIRS=drivers/virgo/cpupooling/virgocloudexec
+make -j 20 O=/home/kashrinivaasan/linux-3.7.8-build-2 modules_install SUBDIRS=drivers/virgo/cpupooling/virgocloudexec
+make -j 20 O=/home/kashrinivaasan/linux-3.7.8-build-2 modules
+make -j 20 O=/home/kashrinivaasan/linux-3.7.8-build-2
+make -j 20 O=/home/kashrinivaasan/linux-3.7.8-build-2 modules_install
+make -j 20 O=/home/kashrinivaasan/linux-3.7.8-build-2 install
 make headers_install
+update-initramfs -uvk 3.7.8
