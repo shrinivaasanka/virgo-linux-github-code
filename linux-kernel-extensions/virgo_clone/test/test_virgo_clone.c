858743c1ab256f63704d468d65de767e16dc02f6
ka_shrinivaasan <ka_shrinivaasan@158c0cd9-b3a7-4c9b-a7e4-89ab9697128b>
2013-07-24 06:02:05 +0000

    Updated test_virgo_clone.c with mangled function name to be sent to virgo kernel service in remote cloud node: ------------------------------------------------------------------------------------------------------------ Mangled name for function prototype "void* virgo_cloud_test(void*)" is sent to virgo_cloudexec kernel service which does upcall to userspace to invoke the function (within libvirgo.so) in remote cloud node. Kernel upcall usermode helper requires mangled names for dlsym(). This also is a unique id for a function. Library libvirgo.so needs to be prebuilt with all functions that need to be on cloud and should be replicated in all cloud nodes. Padded right with 2 whitespaces for ^M removal code in kernel module side.

--- linux-kernel-extensions/virgo_clone/test/test_virgo_clone.c
+++ linux-kernel-extensions/virgo_clone/test/test_virgo_clone.c
@@ -5,6 +5,18 @@
 
 int main()
 {
+	/*
 	long int ret=syscall(350,"int clone_func(void* args)",NULL,0,NULL);
+	*/
+
+	/*
+	Mangled name for function prototype "void* virgo_cloud_test(void*)" is sent to virgo_cloudexec kernel service which does
+	upcall to userspace to invoke the function (within libvirgo.so) in remote cloud node. Kernel upcall usermode helper 
+	requires mangled names for dlsym(). This also is a unique id for a function. Library libvirgo.so needs to be prebuilt
+	with all functions that need to be on cloud and should be replicated in all cloud nodes.
+	Padded right with 2 whitespaces for ^M removal code in kernel module side.
+	- Ka.Shrinivaasan
+	*/
+	long int ret=syscall(350,"_Z16virgo_cloud_testPv  ",NULL,0,NULL);
 	return 0;
 }
