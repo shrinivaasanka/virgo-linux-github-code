865da054d466f6ad72de6b06a7ed811902e01ebd
ka_shrinivaasan <ka_shrinivaasan@158c0cd9-b3a7-4c9b-a7e4-89ab9697128b>
2013-08-05 07:05:47 +0000

    Latest staging.sh with Copyright Notice added

--- staging.sh
+++ staging.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+
+#/***************************************************************************************
+#VIRGO - a linux module extension with CPU and Memory pooling with cloud capabilities
+#
+#Copyright (C) 2009-2013  Ka.Shrinivaasan
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#mail to: ka.shrinivaasan@gmail.com
+#*****************************************************************************************/
+
 cp -r /home/kashrinivaasan/linux-3.7.8/virgo_clone /home/kashrinivaasan/KrishnaiResearch_OpenSource//virgo-linux-code-0/linux-kernel-extensions/
 cp -r /home/kashrinivaasan/linux-3.7.8/drivers/virgo/cpupooling/* /home/kashrinivaasan/KrishnaiResearch_OpenSource//virgo-linux-code-0/linux-kernel-extensions/drivers/virgo/cpupooling
 cp -r /home/kashrinivaasan/linux-3.7.8/include/linux/virgo*h /home/kashrinivaasan/KrishnaiResearch_OpenSource//virgo-linux-code-0/linux-kernel-extensions/include/linux
