73c4b1ef9d44604a60181843bbbbc09f9b23778b
ka_shrinivaasan <ka_shrinivaasan@158c0cd9-b3a7-4c9b-a7e4-89ab9697128b>
2013-08-23 08:14:26 +0000

    Changes done for Multithreaded VIRGO Kernel Service Feature: - new kernel thread and function virgo_client_thread spawned that does the recvfrom and sendto work for each clientsocket that is accepted as parameter in those virgo_ops functions - Updated Copyright Notice

--- linux-kernel-extensions/include/linux/virgocloudexecsvc.h
+++ linux-kernel-extensions/include/linux/virgocloudexecsvc.h
@@ -16,13 +16,22 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-mail to: ka.shrinivaasan@gmail.com
+--------------------------------------------------------------------------------------------------
+Srinivasan Kannan (alias) Ka.Shrinivaasan (alias) Shrinivas Kannan
+Independent Open Source Developer, Researcher and Consultant
+Ph: 9003082186, 9791165980
+Open Source Products Profile(Krishna iResearch): http://sourceforge.net/users/ka_shrinivaasan
+Personal website(research): https://sites.google.com/site/kuja27/
+emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com, kashrinivaasan@live.com
+--------------------------------------------------------------------------------------------------
+
 *****************************************************************************************/
 
 #ifndef _LINUX_VIRGOCLOUDEXECSVC_H
 #define _LINUX_VIRGOCLOUDEXECSVC_H
 
 #include <linux/virgo.h>
+#include <linux/socket.h>
 
 
 int virgo_cloudexec_service(void* args)
@@ -31,11 +40,42 @@ int virgo_cloudexec_service(void* args)
 	try_module_get(virgo_class.m_virgo_owner);
 	while(1)
 	{
-		virgo_ops.virgo_create();
+		printk(KERN_INFO "virgo_cloudexec_service(): virgo_ops.virgo_create() \n");
+		struct socket* clsock = virgo_ops.virgo_create();
+
+		/*
+		Multithreaded VIRGO Kernel Service
+		----------------------------------
+		*/
+		
+		
+		struct task_struct *task;
+		void* args=clsock;
+		int woken_up=0;
+		printk(KERN_INFO "virgo_cloudexec_service(): virgo client thread per request \n");
+                task=kthread_create(virgo_client_thread, (void*)args, "virgo client thread per virgo_clone request");
+		woken_up=wake_up_process(task);
+
+		/*
 		virgo_ops.virgo_recvfrom();
 		virgo_ops.virgo_sendto();
+		*/
 	}
 
+}
+
+/*
+Multithreaded VIRGO Kernel Service
+----------------------------------
+*/
+
+int virgo_client_thread(void* args)
+{
+		struct socket* clientsock=(struct socket*)args;
+		printk(KERN_INFO "virgo_client_thread(): virgo_ops.virgo_recvfrom()\n");
+		virgo_ops.virgo_recvfrom(clientsock);
+		printk(KERN_INFO "virgo_client_thread(): virgo_ops.virgo_sendto()\n");
+		virgo_ops.virgo_sendto(clientsock);
 }
 
 #endif
