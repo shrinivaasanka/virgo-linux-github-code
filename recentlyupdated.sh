eae78df61379a469d519372f3e2cee7de56549a0
ka_shrinivaasan <ka_shrinivaasan@158c0cd9-b3a7-4c9b-a7e4-89ab9697128b>
2013-09-19 13:47:57 +0000

    Updated buildscript.sh for VIRGO MemPool driver build

--- recentlyupdated.sh
+++ recentlyupdated.sh
@@ -3,7 +3,7 @@
 #Simple utility script for finding files modified in last 'n' days
 #- Ka.Shrinivaasan ( Krishna iResearch, kashrinivaasan@gmail.com)
 
-for i in `find . -ctime -10 -type f`
+for i in `find . -ctime -1 -type f`
 do 
 cp --parents -r $i /home/kashrinivaasan/KrishnaiResearch_OpenSource/virgo-linux-code-0/linux-kernel-extensions
 done
