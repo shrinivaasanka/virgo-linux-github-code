3dfbade1adc46037e646fd2c7fc1f00fe15b9d65
ka_shrinivaasan <ka_shrinivaasan@158c0cd9-b3a7-4c9b-a7e4-89ab9697128b>
2013-10-17 14:29:02 +0000

    Updated sscanftest.c with separate pointer for var_sscanf and output in comments. Meanwhile, latest kernel build with same vsscanf code still returns null in virgo_malloc.c as in kern.log below ------------------------------------------------------------------------------------------ Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023661] virgo_mempool_client_thread(): virgo_mempool_ops.virgo_mempool_sendto() Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023665] toAddressString(): address=eea74000, sprintf strAddress=[eea74000] Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023669] virgocloudexec_mempool_sendto(): before kernel_sendmsg() for send buffer: eea74000 Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023700] virgocloudexec_mempool_sendto(): kernel_sendmsg() returns ret: 500 Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023703] virgocloudexec_mempool_sendto(): Shut down Kernel Side Client Socket with SOCK_WAKE_URG after sendmsg Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023725] virgocloudexec_mempool_sendto(): sock_release invoked on client socket Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023733] virgo_malloc() syscall: recv len=500; received message buf: [eea74000] Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023737] virgo_malloc() syscall: received iov.iov_base: Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023742] str_to_addr(): straddr=[eea74000], address scanned ptr=  (null), address scanned voidptr=  (null) Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023745] str_to_addr(): after var_sscanf(): straddr=[eea74000], voidptr_vargs by vsscanf:   (null) Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023749] virgo_malloc() syscall: vtranstable.vtable[i].addr=  (null) Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023754] virgo_malloc() syscall: next_vtable_entry=2, vtranstable.vtable[next_vtable_entry].node_id=2, vtranstable.vtable[next_vtable_entry].addr=  (null), vtranstable.vtable[next_vtable_entry].hstprt->hostip=127.0.0.1, vtranstable.vtable[next_vtable_entry].hstprt->port=30000 Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023758] virgo_malloc() syscall: le32_to_cpus(buf): eea74000 Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023783] BUG: unable to handle kernel NULL pointer dereference at 00000010 Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023789] IP: [<c14ee1da>] sock_release+0x1a/0x80 Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023800] *pdpt = 000000002eb3e001 *pde = 0000000000000000 Oct 17 19:35:08 kashrinivaasan-Inspiron-1545 kernel: [  166.023807] Oops: 0000 [#3] SMP @                                                                                                        @                                                                                                        @                                                                                                                                                                                               41646,1       99%

--- linux-kernel-extensions/virgo_malloc/test/sscanftest.c
+++ linux-kernel-extensions/virgo_malloc/test/sscanftest.c
@@ -39,10 +39,10 @@ userspace sscanf and vsscanf %p modifier testcase
 
 Outputs:
 --------
-kashrinivaasan@kashrinivaasan-Inspiron-1545:~/linux-3.7.8/virgo_malloc/test$ ./sscanftest 
+shrinivaasan@kashrinivaasan-Inspiron-1545:~/linux-3.7.8/virgo_malloc/test$ ./sscanftest 
 p=0x80486d0, str after sprintf: [0x80486d0]
 str=[0x80486d0], p2 after sscanf: 0x80486d0 
-after var_sscanf(): str=[0x80486d0], p2 after sscanf: 0x80486d0 
+after var_sscanf(): str=[0x80486d0], p3 after sscanf: 0x80486d0 
 
 similar sscanf() works for kernel space in virgo_cloud_mempool_kernelspace.ko 
 but doesn't work in virgo_malloc.c and returns null
@@ -56,13 +56,14 @@ int main()
 {
 	const char* p="string";
 	void* p2;
+	void* p3;
 	char str[300];
 	sprintf(str, "%p", p);
 	printf("p=%p, str after sprintf: [%s]\n", p, str);
 	sscanf(str, "%p", (void**)&p2);
 	printf("str=[%s], p2 after sscanf: %p \n", str, p2);
-	var_sscanf(str, "%p", (void**)&p2);
-	printf("after var_sscanf(): str=[%s], p2 after sscanf: %p \n", str, p2);
+	var_sscanf(str, "%p", (void**)&p3);
+	printf("after var_sscanf(): str=[%s], p3 after sscanf: %p \n", str, p3);
 	return 0;
 }
 
