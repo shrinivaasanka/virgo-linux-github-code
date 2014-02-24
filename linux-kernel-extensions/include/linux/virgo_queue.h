2148a69914c70db26b4093c7039c5d9a66a131f3
ka_shrinivaasan <ka_shrinivaasan@158c0cd9-b3a7-4c9b-a7e4-89ab9697128b>
2014-02-24 11:04:27 +0000

    VIRGO queue header file having datastructures for VIRGO Queueing driver prototype

---
+++ linux-kernel-extensions/include/linux/virgo_queue.h
@@ -0,0 +1,112 @@
+/***************************************************************************************
+VIRGO - a linux module extension with CPU and Memory pooling with cloud capabilities
+
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+--------------------------------------------------------------------------------------------------
+Copyright (C):
+Srinivasan Kannan (alias) Ka.Shrinivaasan (alias) Shrinivas Kannan
+Independent Open Source Developer, Researcher and Consultant
+Ph: 9789346927, 9003082186, 9791165980
+Open Source Products Profile(Krishna iResearch): http://sourceforge.net/users/ka_shrinivaasan
+Personal website(research): https://sites.google.com/site/kuja27/
+emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com, kashrinivaasan@live.com
+--------------------------------------------------------------------------------------------------
+
+*****************************************************************************************/
+
+/*
+	VIRGO queue related datastructures
+*/
+
+#ifndef _VIRGO_QUEUE_H_
+#define _VIRGO_QUEUE_H_
+
+#define VIRGO_QUEUE_SZ 10000
+
+#include <linux/ioport.h>
+#include <linux/kobject.h>
+#include <linux/klist.h>
+#include <linux/list.h>
+#include <linux/lockdep.h>
+#include <linux/compiler.h>
+#include <linux/types.h>
+#include <linux/mutex.h>
+#include <linux/pm.h>
+#include <linux/atomic.h>
+#include <linux/ratelimit.h>
+#include <asm/device.h>
+
+#include <linux/kernel.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/module.h>
+#include <linux/errno.h>
+#include <linux/fcntl.h>
+#include <linux/net.h>
+#include <linux/in.h>
+#include <linux/inet.h>
+#include <linux/udp.h>
+#include <linux/tcp.h>
+#include <linux/string.h>
+#include <linux/unistd.h>
+#include <linux/slab.h>
+#include <linux/netdevice.h>
+#include <linux/skbuff.h>
+#include <linux/file.h>
+#include <linux/freezer.h>
+#include <net/sock.h>
+#include <net/checksum.h>
+#include <net/ip.h>
+#include <net/ipv6.h>
+#include <net/tcp.h>
+#include <net/tcp_states.h>
+#include <asm/uaccess.h>
+#include <asm/ioctls.h>
+#include <trace/events/skb.h>
+
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/uio.h>
+#include <linux/unistd.h>
+/*#include <linux/init.h>*/
+
+#include <linux/sunrpc/types.h>
+#include <linux/sunrpc/clnt.h>
+#include <linux/sunrpc/xdr.h>
+#include <linux/sunrpc/msg_prot.h>
+#include <linux/sunrpc/svcsock.h>
+#include <linux/sunrpc/stats.h>
+#include <linux/sunrpc/xprt.h>
+/*#include "sunrpc.h"*/
+#include <linux/sunrpc/xprt.h>
+/*#include "netns.h"*/
+
+
+struct virgo_request
+{
+	char* data;
+	struct virgo_request* next;
+};
+
+int queue_front=0;
+int queue_end=0;
+
+struct virgo_request* virgo_request_queue;
+void init_virgo_queue();
+void push_request(struct virgo_request* req);
+struct virgo_request* pop_request(struct virgo_request* req);
+                                      
+#endif _VIRGO_QUEUE_H_
