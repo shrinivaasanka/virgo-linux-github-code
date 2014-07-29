4cf082f2b563d20c1244cbfe4156581bda6b3211
ka_shrinivaasan <ka_shrinivaasan@158c0cd9-b3a7-4c9b-a7e4-89ab9697128b>
2014-07-29 12:24:10 +0000

    - Updated Makefile with -w flag added for 3.15.5 kernel build. - Updated build generated (3.15.5 kernel) objects and symbol files

--- linux-kernel-extensions/drivers/virgo/cpupooling/virgocloudexec/virgo_cloud_test_kernelspace.mod.c
+++ linux-kernel-extensions/drivers/virgo/cpupooling/virgocloudexec/virgo_cloud_test_kernelspace.mod.c
@@ -4,7 +4,7 @@
 
 MODULE_INFO(vermagic, VERMAGIC_STRING);
 
-struct module __this_module
+__visible struct module __this_module
 __attribute__((section(".gnu.linkonce.this_module"))) = {
 	.name = KBUILD_MODNAME,
 	.init = init_module,
@@ -17,10 +17,10 @@ __attribute__((section(".gnu.linkonce.this_module"))) = {
 static const struct modversion_info ____versions[]
 __used
 __attribute__((section("__versions"))) = {
-	{ 0x8a9dc593, "module_layout" },
-	{ 0x50eedeb8, "printk" },
-	{ 0xb4390f9a, "mcount" },
-	{ 0x952664c5, "do_exit" },
+	{ 0x11c92bdf, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x50eedeb8, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0xb4390f9a, __VMLINUX_SYMBOL_STR(mcount) },
+	{ 0x952664c5, __VMLINUX_SYMBOL_STR(do_exit) },
 };
 
 static const char __module_depends[]
