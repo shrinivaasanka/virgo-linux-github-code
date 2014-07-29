7b54128febbe13df2ab87a4bbcdfb5feb8d4bf05
ka_shrinivaasan <ka_shrinivaasan@158c0cd9-b3a7-4c9b-a7e4-89ab9697128b>
2014-07-29 12:28:33 +0000

    Updated build generated object files and symbols

--- linux-kernel-extensions/drivers/virgo/queuing/virgo_queue.mod.c
+++ linux-kernel-extensions/drivers/virgo/queuing/virgo_queue.mod.c
@@ -4,7 +4,7 @@
 
 MODULE_INFO(vermagic, VERMAGIC_STRING);
 
-struct module __this_module
+__visible struct module __this_module
 __attribute__((section(".gnu.linkonce.this_module"))) = {
 	.name = KBUILD_MODNAME,
 	.init = init_module,
@@ -17,26 +17,25 @@ __attribute__((section(".gnu.linkonce.this_module"))) = {
 static const struct modversion_info ____versions[]
 __used
 __attribute__((section("__versions"))) = {
-	{ 0x8a9dc593, "module_layout" },
-	{ 0xb1278c61, "kmalloc_caches" },
-	{ 0x43a53735, "__alloc_workqueue_key" },
-	{ 0x92c90e22, "queue_work" },
-	{ 0x143054c, "filp_close" },
-	{ 0xc499ae1e, "kstrdup" },
-	{ 0x50eedeb8, "printk" },
-	{ 0xb4390f9a, "mcount" },
-	{ 0x952664c5, "do_exit" },
-	{ 0xe3fe0694, "kingcobra_servicerequest_kernelspace" },
-	{ 0x86a4889a, "kmalloc_order_trace" },
-	{ 0x68d4fad5, "call_usermodehelper_fns" },
-	{ 0xac518073, "kmem_cache_alloc_trace" },
-	{ 0x95a608a3, "filp_open" },
+	{ 0x11c92bdf, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x7f457a3b, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
+	{ 0x43a53735, __VMLINUX_SYMBOL_STR(__alloc_workqueue_key) },
+	{        0, __VMLINUX_SYMBOL_STR(filp_close) },
+	{ 0xc499ae1e, __VMLINUX_SYMBOL_STR(kstrdup) },
+	{ 0x50eedeb8, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0xb4390f9a, __VMLINUX_SYMBOL_STR(mcount) },
+	{ 0xc5fdef94, __VMLINUX_SYMBOL_STR(call_usermodehelper) },
+	{ 0x952664c5, __VMLINUX_SYMBOL_STR(do_exit) },
+	{ 0x86a4889a, __VMLINUX_SYMBOL_STR(kmalloc_order_trace) },
+	{ 0xd3813bef, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
+	{ 0xb2d48a2e, __VMLINUX_SYMBOL_STR(queue_work_on) },
+	{ 0xdb18b84e, __VMLINUX_SYMBOL_STR(filp_open) },
 };
 
 static const char __module_depends[]
 __used
 __attribute__((section(".modinfo"))) =
-"depends=kingcobra_main_kernelspace";
+"depends=";
 
 
 MODULE_INFO(srcversion, "95D8EF1D4CDEBDFB114F725");
