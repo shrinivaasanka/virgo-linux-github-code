7e58df3c96024b7f1c73b1bc3dc86b91477ce09d
ka_shrinivaasan <ka_shrinivaasan@158c0cd9-b3a7-4c9b-a7e4-89ab9697128b>
2014-08-07 06:59:26 +0000

    Changed the extern fields to simple global declarations

--- linux-kernel-extensions/include/linux/virgo_config.h
+++ linux-kernel-extensions/include/linux/virgo_config.h
@@ -1,3 +1,5 @@
-extern int num_cloud_nodes;
+/*extern int num_cloud_nodes;*/
+int num_cloud_nodes;
 
-extern char* node_ip_addrs_in_cloud[3000];
+/*extern char* node_ip_addrs_in_cloud[3000];*/
+char* node_ip_addrs_in_cloud[3000];
