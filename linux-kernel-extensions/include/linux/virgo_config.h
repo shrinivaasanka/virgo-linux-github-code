f58a0e212792fbfa38603d3fc8bcd914d82be6a5
ka_shrinivaasan <ka_shrinivaasan@158c0cd9-b3a7-4c9b-a7e4-89ab9697128b>
2014-08-16 08:10:12 +0000

    Added #ifndef for virgo_config.h which was missing earlier

--- linux-kernel-extensions/include/linux/virgo_config.h
+++ linux-kernel-extensions/include/linux/virgo_config.h
@@ -1,5 +1,10 @@
+#ifndef _VIRGO_CONFIG_H_
+#define _VIRGO_CONFIG_H_
+
 /*extern int num_cloud_nodes;*/
 int num_cloud_nodes;
 
 /*extern char* node_ip_addrs_in_cloud[3000];*/
 char* node_ip_addrs_in_cloud[3000];
+
+#endif
