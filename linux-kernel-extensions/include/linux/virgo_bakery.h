46e192bf78898a371e0b04b1b74efe279ac64c33
ka_shrinivaasan <ka_shrinivaasan@158c0cd9-b3a7-4c9b-a7e4-89ab9697128b>
2014-11-26 10:39:32 +0000

    Initial implementation for VIRGO Cloud Synchronization (cloudsync): - Leslie Lamport's Bakery Algorithm has been added which can be used by the cloud nodes

---
+++ linux-kernel-extensions/include/linux/virgo_bakery.h
@@ -0,0 +1,172 @@
+/************************************************************************************************
+#ASFER - a ruleminer which gets rules specific to a query and executes them (component of iCloud Platform)
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#---------------------------------------------------------------------------------------------------------
+#Copyright (C):
+#Srinivasan Kannan (alias) Ka.Shrinivaasan (alias) Shrinivas Kannan
+#Ph: 9789346927, 9003082186, 9791165980
+#Krishna iResearch Open Source Products Profiles: 
+#http://sourceforge.net/users/ka_shrinivaasan, https://www.openhub.net/accounts/ka_shrinivaasan
+#Personal website(research): https://sites.google.com/site/kuja27/
+#ZODIAC DATASOFT: https://github.com/shrinivaasanka/ZodiacDatasoft
+#emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com, kashrinivaasan@live.com
+#---------------------------------------------------------------------------------------------------------
+**************************************************************************************************/
+
+
+/*
+Experimental - Bakery Algorithm Implementation for VIRGO Cloud
+--------------------------------------------------------------
+
+1.lock():
+	- Each thread i gets a unique number - 1 + max(already assigned)
+	- for all threads k:
+		 spinlock_till([i,num(i)] < [k,num(k)]
+		 { i < k or (i==k and num(i) < num(k)) }
+
+2.unlock():
+	- Discard unique number
+
+For cloud synchronization, above unique id can also be augmented with ip address and timestamp optionally. 
+*/
+
+#ifndef _VIRGO_BAKERY_H_
+#define _VIRGO_BAKERY_H_
+
+#include <linux/ioport.h>
+#include <linux/kobject.h>
+#include <linux/klist.h>
+#include <linux/list.h>
+#include <linux/lockdep.h>
+#include <linux/compiler.h>
+#include <linux/types.h>
+#include <linux/mutex.h>
+#include <linux/pm.h>
+#include <linux/atomic.h>
+#include <linux/ratelimit.h>
+#include <asm/device.h>
+
+/******************************************/
+
+#include <linux/kernel.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/module.h>
+#include <linux/errno.h>
+#include <linux/fcntl.h>
+#include <linux/net.h>
+#include <linux/in.h>
+#include <linux/inet.h>
+#include <linux/udp.h>
+#include <linux/tcp.h>
+#include <linux/string.h>
+#include <linux/unistd.h>
+#include <linux/slab.h>
+#include <linux/netdevice.h>
+#include <linux/skbuff.h>
+#include <linux/file.h>
+#include <linux/freezer.h>
+#include <net/sock.h>
+#include <net/checksum.h>
+#include <net/ip.h>
+#include <net/ipv6.h>
+#include <net/tcp.h>
+#include <net/tcp_states.h>
+#include <asm/uaccess.h>
+#include <asm/ioctls.h>
+#include <trace/events/skb.h>
+
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/uio.h>
+#include <linux/unistd.h>
+/*#include <linux/init.h>*/
+
+#include <linux/sunrpc/types.h>
+#include <linux/sunrpc/clnt.h>
+#include <linux/sunrpc/xdr.h>
+#include <linux/sunrpc/msg_prot.h>
+#include <linux/sunrpc/svcsock.h>
+#include <linux/sunrpc/stats.h>
+#include <linux/sunrpc/xprt.h>
+/*#include "sunrpc.h"*/
+#include <linux/sunrpc/xprt.h>
+/*#include "netns.h"*/
+
+
+
+
+/* Maximum number of concurrent threads that contend for critical section */
+#define MAX_CONCURRENT_THREADS 1000
+
+/* For each thread boolean flag array to indicate if in critical section or not */
+int in_critical_section[MAX_CONCURRENT_THREADS];
+
+/* Token Number assigned by the cloud node service for each client threads */
+int token[MAX_CONCURRENT_THREADS];
+
+int virgobakery_threadfunc(void* args);
+
+int shared=0;
+
+/* 
+Lock and Unlock - 0 <= thread_id < MAX_CONCURRENT_THREADS
+Locking has three phases of entering critical section:
+- Phase 1 : the unique token number is allotted for each thread and thread exits the Phase 1 of critical section
+- Phase 2 : Only threads with unique number enter the third phase and other threads spinwait in second phase. 
+- Phase 3 : Only thread with (thread_id, token[thread_id]) < (i, token[i]) for all threads i gets the critical section and others spinwait
+*/
+
+void bakery_lock(int thread_id)
+{
+
+	/* Phase 1 */	
+	in_critical_section[thread_id]=1;
+	token[thread_id] = 1 + maximum_of(token);
+	in_critical_section[thread_id]=0;
+
+	/* Phase 2 */
+	int i;
+	for(i=0 ; i < MAX_CONCURRENT_THREADS; i++)
+	{
+		while(in_critical_section[i] == 1)
+			;
+	}
+	
+	/* Phase 3 - only thread id(s) with unique numbers */
+	for(i=0 ; i < MAX_CONCURRENT_THREADS; i++)
+	{
+		while((token[thread_id] < token[i]) ||	(token[thread_id] == token[i] && thread_id > i))
+			;
+	}
+}
+
+void bakery_unlock(int thread_id)
+{
+	in_critical_section[thread_id]=0;
+	token[thread_id]=0;
+}
+
+int maximum_of(int* token)
+{
+	int max=-1;
+	int i;
+	for(i=0; i < MAX_CONCURRENT_THREADS; i++)
+	{
+		if(token[i] > max)
+			max=token[i];	
+	}
+	return max;
+}
+
+#endif
