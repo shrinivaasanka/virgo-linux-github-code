e97b875f6fb20197a984c280f41b2079dfccea1d
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2015-08-13 21:44:47 +0530

    VIRGO Linux Kernel 4.1.5 port - utils driver:    - Makefile include paths updated    - updated kernel built sources, objects, symbol files

--- linux-kernel-extensions/drivers/virgo/utils/virgo_generic_kernelsock_client.mod.c
+++ linux-kernel-extensions/drivers/virgo/utils/virgo_generic_kernelsock_client.mod.c
@@ -17,17 +17,17 @@ __attribute__((section(".gnu.linkonce.this_module"))) = {
 static const struct modversion_info ____versions[]
 __used
 __attribute__((section("__versions"))) = {
-	{ 0x5ac9bae1, __VMLINUX_SYMBOL_STR(module_layout) },
-	{ 0x94768c77, __VMLINUX_SYMBOL_STR(kernel_sendmsg) },
+	{ 0x86983313, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0xb855372e, __VMLINUX_SYMBOL_STR(kernel_sendmsg) },
 	{ 0xd0d8621b, __VMLINUX_SYMBOL_STR(strlen) },
-	{ 0x2b701874, __VMLINUX_SYMBOL_STR(sock_create_kern) },
-	{ 0x146bcf26, __VMLINUX_SYMBOL_STR(kernel_connect) },
+	{ 0x187aff44, __VMLINUX_SYMBOL_STR(sock_create_kern) },
+	{ 0x3031a6be, __VMLINUX_SYMBOL_STR(kernel_connect) },
 	{ 0x50eedeb8, __VMLINUX_SYMBOL_STR(printk) },
 	{ 0xb4390f9a, __VMLINUX_SYMBOL_STR(mcount) },
 	{ 0x952664c5, __VMLINUX_SYMBOL_STR(do_exit) },
 	{ 0xdb7305a1, __VMLINUX_SYMBOL_STR(__stack_chk_fail) },
 	{ 0xaccabc6a, __VMLINUX_SYMBOL_STR(in4_pton) },
-	{ 0x42f8b31d, __VMLINUX_SYMBOL_STR(kernel_recvmsg) },
+	{ 0x2fa31741, __VMLINUX_SYMBOL_STR(kernel_recvmsg) },
 	{ 0xe914e41e, __VMLINUX_SYMBOL_STR(strcpy) },
 };
 
