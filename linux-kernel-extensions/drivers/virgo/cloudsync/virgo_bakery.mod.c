9a13cf71cab51383c65498095f3edd53ada06b38
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2015-08-13 20:57:40 +0530

    VIRGO Linux kernel 4.1.5 port - cloudsync bakery algorithm driver:    - Makefile include path changed    - kernel generated sources, objects and symbols

--- linux-kernel-extensions/drivers/virgo/cloudsync/virgo_bakery.mod.c
+++ linux-kernel-extensions/drivers/virgo/cloudsync/virgo_bakery.mod.c
@@ -17,14 +17,14 @@ __attribute__((section(".gnu.linkonce.this_module"))) = {
 static const struct modversion_info ____versions[]
 __used
 __attribute__((section("__versions"))) = {
-	{ 0x5ac9bae1, __VMLINUX_SYMBOL_STR(module_layout) },
-	{ 0x649c402a, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
-	{ 0x6d1a33c1, __VMLINUX_SYMBOL_STR(kthread_create_on_node) },
+	{ 0x86983313, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0xf23db2b3, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
+	{ 0x9aab63c, __VMLINUX_SYMBOL_STR(kthread_create_on_node) },
 	{ 0x50eedeb8, __VMLINUX_SYMBOL_STR(printk) },
 	{ 0xb4390f9a, __VMLINUX_SYMBOL_STR(mcount) },
 	{ 0x952664c5, __VMLINUX_SYMBOL_STR(do_exit) },
-	{ 0x189f8038, __VMLINUX_SYMBOL_STR(wake_up_process) },
-	{ 0x702984bb, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
+	{ 0x13f168f2, __VMLINUX_SYMBOL_STR(wake_up_process) },
+	{ 0x86c90730, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
 };
 
 static const char __module_depends[]
